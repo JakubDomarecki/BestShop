cca7f697772f960cb74a147cb075c16a0a053dcc
kuba86952@wp.pl <kuba86952@wp.pl>
2024-05-30 16:19:01 +0200

    changed vite configuration

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: `@import "./scss/main.scss";`
+        // additionalData: `@import "./scss/main.scss";`
       }
     }
   }
